9869add26f6bb77556c17adfc3f618cb8a431808
JF <15828589290@163.com>
2016-10-27 20:43:16 +0800

    1027update

--- test/test/Program.cs
+++ test/test/Program.cs
@@ -10,10 +10,16 @@ namespace test
     {
         static void Main(string[] args)
         {
-            IList<string> list = new Solution_1().FizzBuzz(15);
-            foreach(String s in list){
-                Console.WriteLine(s);
-            }
+            //打印出一个字符list
+            //IList<string> list = new Solution_1().FizzBuzz(15);
+            //foreach(String s in list){
+            //    Console.WriteLine(s);
+            //}
+
+            //打印出数组中唯一的一个数字
+            int[] number = { 2,1,1 };
+            int m = new Solution_2().SingleNumber(number);
+            Console.WriteLine(m);
         }
         
         
---
+++ test/test/Solution_2.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    class Solution_2
+    {
+        public int SingleNumber(int[] nums)
+        {
+            int result = 0;
+            for (int x = 0; x < nums.Length; ++x)
+            {
+                result = result ^ nums[x];
+            }
+            return result;
+        }
+    }
+}
