8d299b60e700b0b2dcc595d06db684d99d89c14c
JF <15828589290@163.com>
2016-10-28 16:34:51 +0800

    ReverseStringProject

---
+++ ReverseString/ReverseString/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ReverseString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string sentence;
+            sentence = System.Console.ReadLine();
+            char[] chars = sentence.ToCharArray();
+            Array.Reverse(chars);
+            string result = new String(chars);
+        }
+    }
+}
