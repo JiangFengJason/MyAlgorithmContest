3ea95d73691ee0fcb3d324b0e9d58a1d2f2d9d0f
JF <15828589290@163.com>
2016-10-30 17:37:34 +0800

    MaxDepth

---
+++ BinaryTree/BinaryTree/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //重点在Solution.cs和TreeNode.cs里面
+        }
+    }
+}
---
+++ BinaryTree/BinaryTree/Solution.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+    class Solution
+    {
+        public int MaxDepth(TreeNode root) {
+        int deep=0;
+            if(root!=null){
+               int lchilddeep=MaxDepth(root.left);
+               int rchilddeep=MaxDepth(root.right);
+               deep=lchilddeep>=rchilddeep?lchilddeep+1:rchilddeep+1;
+            }
+        return deep;
+        }
+    }
+}
