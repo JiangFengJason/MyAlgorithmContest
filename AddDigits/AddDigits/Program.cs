f1f9e075b89a1928e177aee0868b4888a5b56159
JF <15828589290@163.com>
2016-10-31 17:08:37 +0800

    EasyLoop

---
+++ AddDigits/AddDigits/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddDigits
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int i = new Solution().AddDigits(38);
+            Console.WriteLine(i);
+        }
+    }
+}
---
+++ AddDigits/AddDigits/Solution.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AddDigits
+{
+    class Solution
+    {
+        public int AddDigits(int num)
+        {
+            while (num / 10 != 0)
+            {
+                num = num / 10 + num % 10;
+            }
+            return num;
+        }
+    }
+}
