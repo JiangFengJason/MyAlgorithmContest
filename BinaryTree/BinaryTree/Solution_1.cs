c54b41a332ae357fb19bf33d84e69a294a196765
JF <15828589290@163.com>
2016-11-02 21:46:59 +0800

    1102

---
+++ BinaryTree/BinaryTree/Solution_1.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+    class Solution_1
+    {
+        public TreeNode InvertTree(TreeNode root)
+        {
+            if (root == null)
+                return null;
+            TreeNode ptmpNode = root.left;
+            root.left = InvertTree(root.right);
+            root.right = InvertTree(ptmpNode);
+            return root;
+        }
+    }
+}
