8312e74faf3040aa1ddf9d010e598e60c4935784
JF <15828589290@163.com>
2016-11-02 21:22:24 +0800

    1102

---
+++ Difference/Difference/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Difference
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            char a = new Solution().FindTheDifference("abcd", "abcde");
+            Console.WriteLine(a);
+        }
+    }
+}
---
+++ Difference/Difference/Solution.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Difference
+{
+    class Solution
+    {
+        public char FindTheDifference(string s, string t)
+        {
+            char[] ss = s.ToArray();
+            char[] tt = t.ToArray();
+            int[] count=new int [26];
+            for (int i = 0; i < ss.Length; i++)
+            {
+                count[s[i] - 'a']++;
+            }
+            for (int j = 0; j < tt.Length; j++)
+            {
+                count[t[j] - 'a']--;
+            }
+            for (int i = 0; i < count.Length; i++)
+            {
+                if (count[i] == - 1)
+                    return (char)(i + 'a');
+            }
+            return 'z';
+        }
+    }
+}
