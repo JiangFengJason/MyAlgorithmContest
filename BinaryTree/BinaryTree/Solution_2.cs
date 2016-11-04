014642893d4917e1ff2d3e84e4531160434c30f4
JF <15828589290@163.com>
2016-11-04 22:13:26 +0800

    1104

---
+++ BinaryTree/BinaryTree/Solution_2.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+    class Solution_2
+    {
+        public int sumOfLeftLeaves(TreeNode root)
+        {
+            if (root==null) 
+                return 0;
+            if ((root.left!=null) && (root.left.left==null) && (root.left.right==null))
+            {
+                return root.left.val + sumOfLeftLeaves(root.right);
+            }
+            return sumOfLeftLeaves(root.left) + sumOfLeftLeaves(root.right);
+        }
+    }
+}
