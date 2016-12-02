30b65d66a9c3b61f066fd43b4f7edca142dcf2c9
JF <15828589290@163.com>
2016-12-02 23:08:34 +0800

    1202

--- Difference/Difference/Program.cs
+++ Difference/Difference/Program.cs
@@ -14,9 +14,16 @@ namespace Difference
             //char a = new Solution().FindTheDifference("abcd", "abcde");
             //Console.WriteLine(a);
 
-            bool is_can = new Solution_1().CanConstruct("fffbfg",
-"effjfggbffjdgbjjhhdegh");
-            Console.WriteLine(is_can);
+//            bool is_can = new Solution_1().CanConstruct("fffbfg",
+//"effjfggbffjdgbjjhhdegh");
+//            Console.WriteLine(is_can);
+            int[] nums1={1,2,2,1};
+            int[] nums2={2,2};
+            int[] result = new Solution_2().Intersection(nums1,nums2);
+            foreach (int i in result)
+            {
+                Console.WriteLine(i);
+            }
         }
     }
 }
---
+++ Difference/Difference/Solution_2.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Difference
+{
+    class Solution_2
+    {
+        public int[] Intersection(int[] nums1, int[] nums2)
+        {
+            List<int> m = new List<int>();
+            int n1 = nums1.Length, n2 = nums2.Length;
+            for (int i = 0; i < n1; i++)
+            {
+                if (!m.Contains(nums1[i]))
+                    m.Add(nums1[i]);
+            }
+            List<int> a=new List<int>();
+            for (int i = 0; i < n2; i++)
+            {
+                if (m.Contains(nums2[i]))
+                {
+                    a.Add(nums2[i]);
+                    m.Remove(nums2[i]);
+                }  
+            }
+            int[] res = a.ToArray();
+            return res;
+        }
+    }
+}
