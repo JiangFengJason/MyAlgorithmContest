92f855baf332edd6116c7a0135f071a9ad2bb63a
JF <15828589290@163.com>
2017-05-24 10:41:22 +0800

    0524

---
+++ OSprocess_resource_management/OSprocess_resource_management/ProcessResourceManagement.cs
@@ -0,0 +1,274 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OSprocess_resource_management
+{
+    struct resource {
+	    public int rid ;
+        public int used ;
+        public int wait_request ;
+    };
+    struct PCB {
+        public string pid ;
+        public string type ;
+        public int id ;
+        public int parent ;
+        public int children ;
+        public int younger ;
+        public int older ;
+        public int priority ;
+        public resource[] other_resource;
+    };
+    struct RCB
+    {
+        public string rid ;
+        public int initial ;
+        public int remaining ;
+        public List<int> wait_list;
+    };
+    class ProcessResourceManagement
+    {
+        
+    }
+    class PRM
+    {
+
+        private RCB R1;
+        private RCB R2;
+        private RCB R3;
+        private RCB R4;
+        private int delete_number = -1;
+        List<int>[] rl=new List<int>[3];
+        public int current_running;
+        public RCB[] rcb = new RCB[4];
+        public PCB [] pcb=new PCB[20];
+        //函数集合：
+        public void initPRM()
+        {
+            rl = new List<int>[3];
+            for (int i = 0; i < 20; i++)
+            {
+                pcb[i].id = i;
+            }
+            pcb[0].pid = "init";
+            pcb[0].priority = 0;
+            //rl[0].Add(0);
+            current_running = 0;
+
+            R1.rid = "R1";
+            R1.initial = 1;
+            R1.remaining = 1;
+
+            R2.rid = "R2";
+            R2.initial = 2;
+            R2.remaining = 2;
+
+            R3.rid = "R3";
+            R3.initial = 3;
+            R3.remaining = 3;
+
+            R4.rid = "R4";
+            R4.initial = 4;
+            R4.remaining = 4;
+
+            rcb[0] = R1;
+            rcb[1] = R2;
+            rcb[2] = R3;
+            rcb[3] = R4;
+
+        }
+        public void timeout()
+        {
+            int p = pcb[current_running].priority;
+            rl[p].Remove(current_running);
+            pcb[current_running].type = "ready";
+            rl[p].Add(current_running);
+            scheduler();
+        }
+        public void release(int n, int unit)
+        {
+            release2(n, unit);
+            scheduler();
+        }
+        public void restore()
+        {   
+            for (int i = 1; i < 20; i++)
+            {
+                pcb[i].other_resource = new resource[4];
+                pcb[i].pid = " ";
+                pcb[i].type = "ready";
+                pcb[i].parent = -1;
+                pcb[i].children = -1;
+                pcb[i].older = -1;
+                pcb[i].younger = -1;
+                pcb[i].priority = -1;
+                pcb[i].other_resource[0].rid = -1;
+                pcb[i].other_resource[1].rid = -1;
+                pcb[i].other_resource[2].rid = -1;
+                pcb[i].other_resource[3].rid = -1;
+                pcb[i].other_resource[0].used = 0;
+                pcb[i].other_resource[1].used = 0;
+                pcb[i].other_resource[2].used = 0;
+                pcb[i].other_resource[3].used = 0;
+                pcb[i].other_resource[0].wait_request = 0;
+                pcb[i].other_resource[1].wait_request = 0;
+                pcb[i].other_resource[2].wait_request = 0;
+                pcb[i].other_resource[3].wait_request = 0;
+            }
+            current_running = 0;
+
+            for (int i = 0; i < 4; i++)
+            {
+                rcb[i].wait_list = new List<int>();
+                rcb[i].initial = i + 1;
+                rcb[i].remaining = i + 1;
+                while (rcb[i].wait_list.Count>0)
+                {
+                    rcb[i].wait_list.RemoveAt(0);
+                }
+            }
+            for (int i = 0; i < 3; i++)
+            {
+                rl[i] = new List<int>();
+                while (rl[i].Count>0)
+                {
+                    rl[i].RemoveAt(rl[i].Count-1);
+                }
+            }
+        }
+        public void create(string name, int p) {
+	        for (int i = 1; i < 20; i++) {
+		        if (pcb[i].pid == " ") {
+			        pcb[i].pid = name;
+			        pcb[i].priority = p;
+			        rl[p].Add(pcb[i].id);
+			        pcb[i].parent = current_running;
+			        for (int j = 1; j < 20; j++) {
+				        if (j < i && pcb[j].parent == pcb[i].parent) {
+					        pcb[j].younger = i;
+					        pcb[i].older = j;
+				        }
+			        }
+			        break;
+		        }   
+	        }   
+	        scheduler();
+        }
+        public int scheduler()
+        {
+            //rl = new List<int>[3];
+	        if (rl[2].Count>0) {
+		        current_running = rl[2].First();
+		        pcb[current_running].type = "running";
+                return rl[2].First();
+	        } else if (rl[1].Count>0) {
+                current_running = rl[1].First();
+		        pcb[current_running].type = "running";
+                return rl[1].First();
+	        } else {
+		        current_running = 0;
+		        pcb[current_running].type = "running";
+		        return 0;
+	        }
+        }
+        public void release2(int n, int unit)
+        {
+            pcb[current_running].other_resource[n].used -= unit;
+            rcb[n].remaining += unit;
+            int temp_pcb = rcb[n].wait_list.First();
+            while (temp_pcb != 0 && pcb[temp_pcb].other_resource[n].wait_request <= rcb[n].remaining)
+            {
+                rcb[n].remaining -= pcb[temp_pcb].other_resource[n].wait_request;
+                rcb[n].wait_list.Remove(temp_pcb);
+                pcb[temp_pcb].type = "ready";
+                pcb[temp_pcb].other_resource[n].used += pcb[temp_pcb].other_resource[n].wait_request;
+                rl[pcb[temp_pcb].priority].Add(temp_pcb);
+                temp_pcb = rcb[n].wait_list.FirstOrDefault();
+            }
+        }
+        public void destroy(int n) {
+	        for (int i = 0; i < 4; i++) {
+		        if (pcb[n].other_resource[i].used != 0) {
+			        release2(i ,pcb[n].other_resource[i].used);
+			        pcb[n].other_resource[i].rid = -1;
+			        pcb[n].other_resource[i].used = 0;
+		        }
+	        }
+	        if (pcb[n].type == "ready" || pcb[n].type == "running") {
+		        int p = pcb[n].priority;
+		        rl[p].Remove(n);
+	        }
+	        else if ((pcb[n].type).CompareTo("blocked") == 0) {
+		        for (int i = 0; i < 4; i++) {
+			        rcb[i].wait_list.Remove(n);
+		        }
+	        }
+	        for (int i = 0; i < 20; i++) {
+		        if (pcb[i].parent == n) {
+			        destroy(pcb[i].id);
+		        }
+		        if (pcb[i].id == n) {
+			        pcb[i].pid = " ";
+			        pcb[i].type = "ready";
+			        pcb[i].parent = -1;
+			        pcb[i].children = -1;
+			        pcb[i].older = -1;
+			        pcb[i].younger = -1;
+			        pcb[i].priority = -1;
+			        pcb[i].other_resource[0].rid = -1;
+			        pcb[i].other_resource[1].rid = -1;
+			        pcb[i].other_resource[2].rid = -1;
+			        pcb[i].other_resource[3].rid = -1;
+			        pcb[i].other_resource[0].used = 0;
+			        pcb[i].other_resource[1].used = 0;
+			        pcb[i].other_resource[2].used = 0;
+			        pcb[i].other_resource[3].used = 0;
+			        pcb[i].other_resource[0].wait_request = 0;
+			        pcb[i].other_resource[1].wait_request = 0;
+			        pcb[i].other_resource[2].wait_request = 0;
+			        pcb[i].other_resource[3].wait_request = 0;
+		        }
+		        if (pcb[i].older == n) {
+			        pcb[i].older = -1;
+		        }
+		        if (pcb[i].younger == n) {
+			        pcb[i].younger = -1;
+		        }
+	        }
+	        scheduler();
+        }
+        public int contain(string name) {
+	        for (int i = 0; i < 20; i++) {
+		        if (name.CompareTo(pcb[i].pid) == 0) {
+			        return i;
+		        }
+	        }
+	        return -1;
+        }
+        public void request(int n, int unit)
+        {
+            if (rcb[n].remaining >= unit)
+            {
+                rcb[n].remaining = rcb[n].remaining - unit;
+                pcb[current_running].other_resource[n].rid = n;
+                pcb[current_running].other_resource[n].used += unit;
+            }
+            else
+            {
+                pcb[current_running].type = "blocked";
+                pcb[current_running].other_resource[n].wait_request += unit;
+                rcb[n].wait_list.Add(current_running);
+                rl[pcb[current_running].priority].Remove(current_running);
+            }
+            scheduler();
+        }
+    }
+    class LN
+    {
+        
+    }
+    
+}
---
+++ OSprocess_resource_management/OSprocess_resource_management/Program.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OSprocess_resource_management
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string inputPath;
+            string outputPath;
+            string line;
+            string[] lines;
+            PRM prm=new PRM();
+            prm.initPRM();
+            inputPath = "D:/gitfile/OSprocess_resource_management/input.txt";
+            outputPath = "D:/gitfile/OSprocess_resource_management/output.txt";
+            FileStream fs = new FileStream(outputPath, FileMode.Create, FileAccess.Write);
+            fs.Close();
+            StreamReader sr = new StreamReader(inputPath, Encoding.Default);
+            StreamWriter sw = new StreamWriter(outputPath, true, Encoding.Default);
+            //按行读取文件
+            while(sr.Peek()>-1)
+            {
+                line = sr.ReadLine();
+                lines = line.Split(' ');
+                if (lines[0] == "init")
+                {
+                    prm.restore();
+                    Console.WriteLine(prm.pcb[prm.current_running].pid);
+                    sw.WriteLine(prm.pcb[prm.current_running].pid);
+                    sw.Flush();
+                }
+                else if (lines[0] == "quit")
+                {
+                    break;
+                }
+                else if (lines[0] == "cr")
+                {
+                    prm.create(lines[1], int.Parse(lines[2]));
+                    Console.WriteLine(prm.pcb[prm.current_running].pid);
+                    sw.WriteLine(prm.pcb[prm.current_running].pid);
+                    sw.Flush();
+                }
+                else if (lines[0] == "de")
+                {
+                    int t = prm.contain(lines[1]);
+                    prm.destroy(t);
+					Console.WriteLine(prm.pcb[prm.current_running].pid);
+                    sw.WriteLine(prm.pcb[prm.current_running].pid);
+                    sw.Flush();
+                }
+                else if (lines[0] == "req")
+                {
+                    if (lines[1] == "R1"&&int.Parse(lines[2])==1)
+                    {
+                        prm.request(0, int.Parse(lines[2]));
+					    Console.WriteLine(prm.pcb[prm.current_running].pid);
+                        sw.WriteLine(prm.pcb[prm.current_running].pid);
+                        sw.Flush();
+                    }
+                    else if (lines[1] == "R2" && (0 < int.Parse(lines[2]) && int.Parse(lines[2]) <= 2))
+                    {
+                        prm.request(1, int.Parse(lines[2]));
+                        Console.WriteLine(prm.pcb[prm.current_running].pid);
+                        sw.WriteLine(prm.pcb[prm.current_running].pid);
+                        sw.Flush();
+                    }
+                    else if (lines[1] == "R3" && (0 < int.Parse(lines[2]) && int.Parse(lines[2]) <= 3))
+                    {
+                        prm.request(2, int.Parse(lines[2]));
+                        Console.WriteLine(prm.pcb[prm.current_running].pid );
+                        sw.WriteLine(prm.pcb[prm.current_running].pid);
+                        sw.Flush();
+                    }
+                    else if (lines[1] == "R4" && (0 < int.Parse(lines[2]) && int.Parse(lines[2]) <= 4))
+                    {
+                        prm.request(3, int.Parse(lines[2]));
+                        Console.WriteLine(prm.pcb[prm.current_running].pid);
+                        sw.WriteLine(prm.pcb[prm.current_running].pid);
+                        sw.Flush();
+                    }
+                }
+                else if (lines[0] == "rel")
+                {
+                    if (lines[1] == "R1" && int.Parse(lines[2]) == 1)
+                    {
+                        if (prm.pcb[prm.current_running].other_resource[0].used >= int.Parse(lines[2]))
+                        {
+                            prm.release(0, int.Parse(lines[2]));
+                            Console.WriteLine(prm.pcb[prm.current_running].pid );
+                            sw.WriteLine(prm.pcb[prm.current_running].pid);
+                            sw.Flush();
+					    }
+                    }
+                    else if (lines[1] == "R2" && (0 < int.Parse(lines[2]) && int.Parse(lines[2]) <= 2))
+                    {
+                        if (prm.pcb[prm.current_running].other_resource[1].used >= int.Parse(lines[2]))
+                        {
+                            prm.release(1, int.Parse(lines[2]));
+                            Console.WriteLine(prm.pcb[prm.current_running].pid );
+                            sw.WriteLine(prm.pcb[prm.current_running].pid);
+                            sw.Flush();
+                        }
+                    }
+                    else if (lines[1] == "R3" && (0 < int.Parse(lines[2]) && int.Parse(lines[2]) <= 3))
+                    {
+                        if (prm.pcb[prm.current_running].other_resource[2].used >= int.Parse(lines[2]))
+                        {
+                            prm.release(2, int.Parse(lines[2]));
+                            Console.WriteLine(prm.pcb[prm.current_running].pid );
+                            sw.WriteLine(prm.pcb[prm.current_running].pid);
+                            sw.Flush();
+                        }
+                    }
+                    else if (lines[1] == "R4" && (0 < int.Parse(lines[2]) && int.Parse(lines[2]) <= 4))
+                    {
+                        if (prm.pcb[prm.current_running].other_resource[3].used >= int.Parse(lines[2]))
+                        {
+                            prm.release(3, int.Parse(lines[2]));
+                            Console.WriteLine(prm.pcb[prm.current_running].pid );
+                            sw.WriteLine(prm.pcb[prm.current_running].pid);
+                            sw.Flush();
+                        }
+                    }
+                }
+                else if (lines[0] == "to")
+                {
+                    prm.timeout();
+				    Console.WriteLine( prm.pcb[prm.current_running].pid );
+                    sw.WriteLine(prm.pcb[prm.current_running].pid);
+                    sw.Flush();
+                }
+            }
+            sw.Close();
+        }
+    }
+}
